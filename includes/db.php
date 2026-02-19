883a5b5539e564a54d8a14719e25c5f578791473
Alexander <angelalexander.gs2@gmail.com>
2026-02-18 20:33:23 -0600

    p

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "root", "password", "ferrepos");
+$conexion = new mysqli("localhost", "root", "", "ferrepos");
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
